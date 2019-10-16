c84b2fe378aa33e65e714918eaf3a24b8dc32b8e
Саша Гужавин <sashaguzavin@gmail.com>
2019-10-17 01:42:53 +0300

    good

---
+++ GuzhavinA_HW2.9/Model.swift
@@ -0,0 +1,45 @@
+//
+//  Model.swift
+//  GuzhavinA_HW2.9
+//
+//  Created by Саша Гужавин on 16/10/2019.
+//  Copyright © 2019 Гужавин Александр. All rights reserved.
+//
+
+import Foundation
+
+struct Animation {
+    
+    var animation = [
+        "Shake",
+        "Pop",
+        "Morph",
+        "Squeeze",
+        "Wobble",
+        "Swing",
+        "FlipX",
+        "FlipY",
+        "Fall",
+        "SqueezeLeft",
+        "SqueezeRight",
+        "SqueezeDown",
+        "SqueezeUp",
+        "SlideLeft",
+        "SlideRight",
+        "SlideDown",
+        "SlideUp",
+        "FadeIn",
+        "FadeOut",
+        "FadeInLeft",
+        "FadeInRight",
+        "FadeInDown",
+        "FadeInUp",
+        "ZoomIn",
+        "ZoomOut",
+        "Flash"
+    ]
+    
+    
+    
+    
+}
--- GuzhavinA_HW2.9/ViewController.swift
+++ GuzhavinA_HW2.9/ViewController.swift
@@ -6,15 +6,94 @@
 //  Copyright © 2019 Гужавин Александр. All rights reserved.
 //
 
-import UIKit
+import Spring
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var viewAnimation: SpringView!
+    @IBOutlet var lableView: [UILabel]!
+    @IBOutlet weak var buttionAnimation: UIButton!
+    
+    var number: [CGFloat] = [2, 3, 4, 5, 6]
+    var animation = [
+        "shake",
+        "pop",
+        "morph",
+        "squeeze",
+        "wobble",
+        "swing",
+        "flipX",
+        "flipY",
+        "fall",
+        "squeezeLeft",
+        "squeezeRight",
+        "squeezeDown",
+        "squeezeUp",
+        "slideLeft",
+        "slideRight",
+        "slideDown",
+        "slideUp",
+        "fadeIn",
+        "fadeOut",
+        "fadeInLeft",
+        "fadeInRight",
+        "fadeInDown",
+        "fadeInUp",
+        "foomIn",
+        "foomOut",
+        "flash"
+    ]
+    var animationCurve = [
+        "easeIn",
+        "easeOut",
+        "easeInOut",
+        "linear",
+        "spring",
+        "easeInSine",
+        "easeOutSine",
+        "easeInOutSine",
+        "easeInQuad",
+        "easeOutQuad",
+        "easeInOutQuad",
+        "easeInCubic",
+        "easeOutCubic",
+        "easeInOutCubic",
+        "easeInQuart",
+        "easeOutQuart",
+        "easeInOutQuart",
+        "easeInQuint",
+        "easeOutQuint",
+        "easeInOutQuint",
+        "easeInExpo",
+        "easeOutExpo",
+        "easeInOutExpo",
+        "easeInCirc",
+        "easeOutCirc",
+        "easeInOutCirc",
+        "easeInBack",
+        "easeOutBack",
+        "easeInOutBack"
+    ]
+    
+    @IBAction func goAnimation() {
+        
+        viewAnimation.animation = animation.first!
+        viewAnimation.curve = animationCurve.first!
+        viewAnimation.force = number.first!
+        viewAnimation.duration = number.first! - 1
+        
+        
+        lableView[0].text = "Animation = \(animation.first!)"
+        lableView[1].text = "Curve = \(animationCurve.first!)"
+        lableView[2].text = "Force = \(viewAnimation.force.binade)"
+        lableView[3].text = "Duration = \(viewAnimation.duration.binade)"
+        
+        animation.shuffle()
+        animationCurve.shuffle()
+        number.shuffle()
+        
+        buttionAnimation.setTitle(animation.first!, for: .normal)
+        
+        viewAnimation.animate()
     }
-
-
 }
-
